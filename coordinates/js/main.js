324ffbd6e898274b6966ed5a8ad1d7d11ed83158
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-02-11 15:25:14 +0100

    coordinates: mouse position detection

---
+++ coordinates/js/main.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function() {
+  var canvas = document.querySelector('#area');
+  canvas.width = document.width;
+  canvas.height = document.height;
+  var ctx = canvas.getContext('2d');
+  ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+  var mouseHandler = function(event) {
+    var coordinates = document.querySelector('#coordinates');
+    coordinates.innerHTML = 'X: ' + event.clientX + ', Y: ' + event.clientY;
+  };
+  canvas.addEventListener('mouseover', mouseHandler)
+  canvas.addEventListener('mousemove', mouseHandler);
+});
